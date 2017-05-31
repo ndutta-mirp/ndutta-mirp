2f623c7e6b8f61bf9b75c82bac257e87b850ccae
ndutta-mirp <ndutta-mirp@mit.edu>
2017-05-31 06:55:18 +0530

    Day 3 programs

---
+++ prog5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=1,a;
+    cout<<"Enter a number"<<endl;
+cin>>a;
+    for (n=1;n<=10;n+=1){
+       cout<<n<<"*"<<a<<"="<<(n*a)<<endl;
+}
+return 0;
+}
