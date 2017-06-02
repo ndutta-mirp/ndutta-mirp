272bf095c8e70c714e9ed3bdbf0848eed010ce8e
Nishan Datta <ndutta-mirp@mit.edu>
2017-06-02 14:09:19 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -4,11 +4,11 @@ int main() {
 	int N;
 	cout<<"Enter a number"<<endl;
 	cin>>N;
-	for(int i=0;i<N;i++){
-	   for(int a=i;a<=i;a++){
-	   cout<<"*"<<endl;
+	for(int i=1;i<=N;i++){
+	    cout<<""<<endl;
+	   for(int a=1;a<=i;a++){
+	   cout<<"*";
 	   }
 	}
 return 0;
 }
-	
